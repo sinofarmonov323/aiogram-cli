4193a61d0c6a85538418770e6c983370b6e9e42d
sinofarmonov323 <farmonovsino4@gmail.com>
2025-09-01 16:08:38 +0500

    updated

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="aiogram_cli3",
-    version="1.0.7",
+    name="aiogram_cli",
+    version="1.0.0",
     packages=find_packages(),
     # install_requires=[
         # "watchdog"
     # ],
     url="https://github.com/sinofarmonov323/aiogram-cli",
-    author="https://t.me/python_dev323",
+    author="https://t.me/jackson_rodger",
     description="bu aiogram kutubxonasi uchun aiogram cli (command line tool)",
     entry_points={
         'console_scripts': [
-            'aiogram-cli = aiogram_cli.cli:main',
+            'aiogram = aiogram_cli.cli:main',
         ],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
+    ]
 )
